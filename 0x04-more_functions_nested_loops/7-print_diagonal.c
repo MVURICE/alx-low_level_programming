5d6d81f1ea782aba763b5093c4c502c30dfc6427
MVURICE <64409721+MVURICE@users.noreply.github.com>
2022-07-07 21:48:35 +0200

    code fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,17 @@
  */
 void print_diagonal(int n)
 {
-    int i, w;
-    
-    if (n < 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (i = 0; i < n; i++)
-        {
-            for (w = 0; w < i; w++)
-            {
-                _putchar(32);
-            }
-            _putchar(92);
-            _putchar('\n');
-        }
-    }
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
 }
